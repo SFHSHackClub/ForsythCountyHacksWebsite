95a8e77805d880625ca4836a0d6d4a657022a278
Christian Dutton <christianedutton@gmail.com>
2023-01-28 11:55:47 -0500

    Updated Head

--- pages/index.js
+++ pages/index.js
@@ -43,10 +43,10 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>Forsyth County Hacks</title>
         <meta name="description" content="something" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/listArrow.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
             <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400&display=swap" rel="stylesheet"/>
